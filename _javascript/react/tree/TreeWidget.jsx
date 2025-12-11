13679ea3bd471e7ef790d365b76f015f34f0d6e4
Jack Zhang <jack.zhang@ringcentral.com>
2025-12-11 11:43:33 +0800

    update

--- _javascript/react/tree/TreeWidget.jsx
+++ _javascript/react/tree/TreeWidget.jsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import { Tree } from 'antd';
-import 'antd/dist/reset.css';
-import { FolderOutlined, FileOutlined } from '@ant-design/icons';
+import React from "react";
+import { Tree, ConfigProvider } from "antd";
+import "antd/dist/reset.css";
+import { FolderOutlined, FileOutlined } from "@ant-design/icons";
 
 export default function TreeWidget({ data }) {
-
   // 将你的 JSON 转换为 AntD Tree 可用格式
   const convertTreeData = (nodes) => {
-    return nodes.map(node => {
-      if (node.type === 'file' && node.url) {
+    return nodes.map((node) => {
+      if (node.type === "file" && node.url) {
         return {
           title: <a href={node.url}>{node.name}</a>, // 点击跳转
           key: node.url,
@@ -20,6 +19,7 @@ export default function TreeWidget({ data }) {
           title: <b>{node.name}</b>,
           icon: <FolderOutlined />,
           key: node.name,
+          selectable: false,
           children: node.children ? convertTreeData(node.children) : []
         };
       }
@@ -28,5 +28,27 @@ export default function TreeWidget({ data }) {
 
   const c = convertTreeData(data);
   console.log(c);
-  return <Tree treeData={c} defaultExpandAll showIcon />;
+  return (
+    <ConfigProvider
+      theme={{
+        components: {
+          Tree: {
+            directoryNodeSelectedBg: '#ebebebff'
+          },
+        }
+      }}
+    >
+      <Tree.DirectoryTree
+        treeData={c}
+        defaultExpandAll
+        showIcon
+        onSelect={(selectedKeys, info) => {
+          const url = selectedKeys[0]; // key 就是我们之前设置的 url 或 name
+          if (url && info.node.isLeaf) {
+            window.location.href = url;
+          }
+        }}
+      />
+    </ConfigProvider>
+  );
 }
